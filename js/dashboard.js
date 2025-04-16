f3654821e2cabcd30fd9051bdd90bd8897c3b5e7
Bo Banducci <bobanducci90@gmail.com>
2025-04-16 11:22:19 -0500

    Querying for user's games

---
+++ js/dashboard.js
@@ -0,0 +1,22 @@
+const token = localStorage.getItem("token");
+if (!token) window.location.href = "/login.html";
+
+fetch("https://tcg-world-backend-production.up.railway.app/api/me", {
+  headers: { Authorization: `Bearer ${token}` }
+})
+  .then(res => res.json())
+  .then(data => {
+	if (data.email) {
+	  document.getElementById("email").innerText = `Logged in as: ${data.email}`;
+	} else {
+	  window.location.href = "/login.html";
+	}
+  });
+  
+fetch("https://tcg-world-backend-production.up.railway.app/api/games", {
+	headers: { Authorization: `Bearer ${token}` }
+})
+	.then (res => res.json())
+	.then (data => {
+		document.getElementById("games").innerText = `Your games: ${data}`;
+	});

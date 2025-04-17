bcae748ad26a73cd0c0290ec807b00377723849b
Bo Banducci <bobanducci90@gmail.com>
2025-04-16 19:16:05 -0500

    adding card upload

--- js/game.js
+++ js/game.js
@@ -13,3 +13,40 @@ fetch(`https://tcg-world-backend-production.up.railway.app/api/games/${gameId}`,
     document.getElementById("gameName").innerText = data.gameName;
     document.getElementById("cardCount").innerText = `Cards: ${data.cardCount}`;
   });
+
+document.getElementById("uploadBtn").addEventListener("click", async () => {
+const input = document.getElementById("cardUploader");
+const files = input.files;
+const status = document.getElementById("uploadStatus");
+
+if (files.length === 0) {
+    status.innerText = "No files selected.";
+    return;
+}
+
+const token = localStorage.getItem("token");
+const params = new URLSearchParams(window.location.search);
+const gameId = params.get("gameId");
+
+for (const file of files) {
+    const formData = new FormData();
+    formData.append("image", file);
+    formData.append("name", file.name);
+
+    const res = await fetch(`https://tcg-world-backend-production.up.railway.app/api/games/${gameId}/cards`, {
+    method: "POST",
+    headers: {
+        Authorization: `Bearer ${token}`
+    },
+    body: formData
+    });
+
+    if (res.ok) {
+        status.innerText = `Uploaded files`;
+    } else {
+        const err = await res.json();
+        status.innerText = `Failed to upload ${file.name}: ${err.error}`;
+    }
+}
+});
+
